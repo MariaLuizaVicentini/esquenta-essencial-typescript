4bc35d942bfb9b6eb265cf50635b6222b103cd36
malu vicentini <maria@multi360.com.br>
2026-08-26 21:45:48 -0300

    revisao(objeto)

--- ts/src/tipos/objetos/objeto.ts
+++ ts/src/tipos/objetos/objeto.ts
@@ -1,9 +1,35 @@
-const pessoa: { nome: string; idade: number } = {
-    nome: 'Maria',
-    idade: 30,
+// const pessoa: { nome: string; idade: number } = {
+//     nome: 'Maria',
+//     idade: 30,
+// };
+
+// pessoa.idade = 31;
+
+// console.log(pessoa.nome);
+// console.log(pessoa.idade);
+
+
+const usuario = {
+    nome: 'Malu',
+    idade: 25,
+    ativo: true
 };
+console.log(usuario)
 
-pessoa.idade = 31;
+usuario.nome = 'Joao'
+console.log(usuario) // É possivel reatribuir o valor de uma propriedade
 
-console.log(pessoa.nome);
-console.log(pessoa.idade);
+
+type Usuario {
+    nome: string,
+    idade: number,
+    ativo: boolean,
+}
+
+const usuarioType: Usuario = {
+    nome: 'malu User type',
+    idade: 25,
+    ativo: true
+}
+
+
