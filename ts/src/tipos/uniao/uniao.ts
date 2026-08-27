b10f30122c7a4cbe813c2d0b5daefca8a9b6bc72
malu vicentini <maria@multi360.com.br>
2026-08-26 21:33:45 -0300

    revisao(union-type)

--- ts/src/tipos/uniao/uniao.ts
+++ ts/src/tipos/uniao/uniao.ts
@@ -1,5 +1,25 @@
 let id: string | number = 5;
-console.log(id);
 
 id = 'f4a460f0-0b3b-4b3b-8b3b-0b3b4b3b8b3b';
-console.log(id);
+
+// id = true; // erro
+
+
+let status: 'ativo' | 'inativo';
+
+status = 'ativo';
+status = 'inativo';
+// status = 'pendente'; // erro
+
+
+type Usuario = {
+    nome: string,
+    tel: string | null;
+};
+
+const usuario_01: Usuario = {
+    nome: 'Maluzeira',
+    tel: null,
+}
+
+
